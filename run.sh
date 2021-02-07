93419884f10da9cf29265126704c6a3c08453b95
Aggelos Aggelis <a.aggelis@satways.net>
2021-02-07 19:16:51 +0200

    added db data

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker build . -t my_mongo_image       #Comment this line if you do not want to build each time
 
 export MSYS_NO_PATHCONV=1
-docker run -d --name my_mongo_container -p 27017:27017 -it --rm -v ~/mongo/data:/data/db my_mongo_image
+docker run -d --name my_mongo_container -p 27017:27017 -it --rm -v ${PWD}/mongo/data:/data/db my_mongo_image
